07948f18fd28ada6818856b6b9223012f1b45f42
TOKI Yoshinori <toki@freedom.ne.jp>
2019-08-27 14:23:29 +0900

    可能なら丸括弧を省略する

--- lib/japanize/core.rb
+++ lib/japanize/core.rb
@@ -3,14 +3,14 @@
 class Module
   alias 別名 alias_method
 
-  別名(:定義,              :define_method)
-  別名(:定数は定義済みか?, :const_defined?)
-  別名(:定数を取得,        :const_get)
-  別名(:定数を設定,        :const_set)
-  別名(:部で評価,          :module_eval)
-  別名(:組で評価,          :class_eval)
-  別名(:秘密,              :private)
-  別名(:部の関数,          :module_function)
+  別名 :定義,              :define_method
+  別名 :定数は定義済みか?, :const_defined?
+  別名 :定数を取得,        :const_get
+  別名 :定数を設定,        :const_set
+  別名 :部で評価,          :module_eval
+  別名 :組で評価,          :class_eval
+  別名 :秘密,              :private
+  別名 :部の関数,          :module_function
 end
 
 module Kernel
@@ -72,17 +72,17 @@ module Japanize
       private 名前
     }
 
-    別名(:付加,           :include)
-    別名(:限定拡張を使用, :using)
+    別名 :付加,           :include
+    別名 :限定拡張を使用, :using
   }
 
-  日本語の定数(:日本語化, ::Japanize)
-  日本語の定数(:核,       ::Kernel)
-  日本語の定数(:部,       ::Module)
-  日本語の定数(:組,       ::Class)
-  日本語の定数(:物,       ::Object)
-  日本語の定数(:記号,     ::Symbol)
-  日本語の定数(:文字列,   ::String)
+  日本語の定数 :日本語化, ::Japanize
+  日本語の定数 :核,       ::Kernel
+  日本語の定数 :部,       ::Module
+  日本語の定数 :組,       ::Class
+  日本語の定数 :物,       ::Object
+  日本語の定数 :記号,     ::Symbol
+  日本語の定数 :文字列,   ::String
 
   定義(:部の定義) {|部, &塊|
     case (部)
--- lib/japanize/test_unit.rb
+++ lib/japanize/test_unit.rb
@@ -5,14 +5,14 @@ require 'japanize/core'
 要求 'test/unit'
 
 部(日本語化) {
-  日本語の定数(:試験単位, Test::Unit)
+  日本語の定数 :試験単位, Test::Unit
 
   部(試験単位) {
-    日本語の定数(:確認手段, Test::Unit::Assertions)
+    日本語の定数 :確認手段, Test::Unit::Assertions
 
     部(確認手段) {
-      別名(:確認,             :assert)
-      別名(:等しいことを確認, :assert_equal)
+      別名 :確認,             :assert
+      別名 :等しいことを確認, :assert_equal
     }
 
     組(:試験事例, 原型: Test::Unit::TestCase) {
@@ -22,8 +22,8 @@ require 'japanize/core'
       定義(:破棄) {}
 
       特異組{
-        別名(:資料, :data)
-        別名(:試験, :test)
+        別名 :資料, :data
+        別名 :試験, :test
       }
     }
   }
