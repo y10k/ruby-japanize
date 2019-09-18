e9a613aa1235f0feadd780ff2b1dbbf53e41c388
TOKI Yoshinori <toki@freedom.ne.jp>
2019-09-18 16:17:12 +0900

    例外や失敗の操作を日本語化する
    固有の操作(メソッド)を持つ例外や失敗を対象とする。
    
    操作(メソッド)の日本語化の対象:
    - 配置の失敗 (LoadError)
    - シグナルの例外 (SignalException)
    - 脱出を補足できないため失敗 (UncaughtThrowError)
    - キーの照合に失敗 (KeyError)
    - 繰り返しの停止 (UncaughtThrowError)
    - 局所的な跳躍に失敗 (LocalJumpError)
    - 名前解決に失敗 (NameError)
    - 操作が無いため失敗 (NoMethodError)
    - システムコールの失敗 (SystemCallError)
    - システムの終了 (SystemExit)
    - 文字符号化方式::バイト列が不正なため失敗 (Encoding::InvalidByteSequenceError)
    - 文字符号化方式::変換後の文字が存在しないため失敗 (Encoding::UndefinedConversionError)

---
+++ lib/japanize/errors.rb
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+require 'japanize/core'
+
+要求 'japanize/base'
+要求 'japanize/names'
+
+組(配置の失敗) {
+  別名 :ファイルへの経路, :path
+}
+
+組(シグナルの例外) {
+  別名 :シグナルの名前, :signm
+  別名 :シグナルの番号, :signo
+}
+
+組(脱出を捕捉できないため失敗) {
+  別名 :目印,           :tag
+  別名 :値,             :value
+  # 別名 :文字列に変換, :to_s    # 物で定義済み
+}
+
+組(キーの照合に失敗) {
+  別名 :キー,   :key
+  別名 :受信先, :receiver
+}
+
+組(繰り返しの停止) {
+  別名 :結果, :result
+}
+
+組(局所的な跳躍に失敗) {
+  別名 :終了値, :exit_value
+  別名 :原因,   :reason
+}
+
+組(名前解決に失敗) {
+  別名 :局所変数の一覧, :local_variables
+  別名 :名前,           :name
+  別名 :受信先,         :receiver
+  # 別名 :文字列に変換, :to_s    # 物で定義済み
+}
+
+組(操作が無いため失敗) {
+  別名 :引数,                    :args
+  別名 :秘密の形式で呼ばれたか?, :private_call?
+}
+
+組(システムコールの失敗) {
+  別名 :失敗の種類を表す番号, :errno
+}
+
+組(システムの終了) {
+  別名 :状態,        :status
+  別名 :成功したか?, :success?
+}
+
+組(文字符号化方式::バイト列が不正なため失敗) {
+  別名 :変換先の文字符号化方式,       :destination_encoding
+  別名 :変換先の文字符号化方式の名前, :destination_encoding_name
+  別名 :不正なバイト列,               :error_bytes
+  別名 :入力が不完全か?,              :incomplete_input?
+  別名 :読み直すバイト列,             :readagain_bytes
+  別名 :変換元の文字符号化方式,       :source_encoding
+  別名 :変換元の文字符号化方式の名前, :source_encoding_name
+}
+
+組(文字符号化方式::変換後の文字が存在しないため失敗) {
+  別名 :変換先の文字符号化方式,       :destination_encoding
+  別名 :変換先の文字符号化方式の名前, :destination_encoding_name
+  別名 :不正な文字,                   :error_char
+  別名 :変換元の文字符号化方式,       :source_encoding
+  別名 :変換元の文字符号化方式の名前, :source_encoding_name
+}
+
+# Local Variables:
+# mode: Ruby
+# indent-tabs-mode: nil
+# End:
