6dcc75c39780e6293c1c6ee19ca7d9db11885088
TOKI Yoshinori <toki@freedom.ne.jp>
2019-12-29 09:31:54 +0900

    基本的な部/組の操作: 警告の抑止が不要になったので削除する (#9)
    Github Issue: #9

--- test/test_base.rb
+++ test/test_base.rb
@@ -4,7 +4,6 @@ require 'japanize/core'
 
 要求 'japanize/base'
 要求 'japanize/test_unit'
-要求 'warning'
 
 部(日本語化) {
   部(:試験) {
@@ -536,9 +535,6 @@ require 'japanize/core'
       試験('引数の転送で名前付き引数と混同せずに連想配列を渡せることを確認') {
         空の連想配列 = {}
 
-        警告の出力.ignore(/: warning: The last argument is used as the keyword parameter\n\z/)
-        警告の出力.ignore(/: warning: for method defined here\n\z/)
-        警告の出力.ignore(/: warning: The keyword argument is passed as the last hash parameter\n\z/)
         ある物 = 組.新規(物) {|ある組|
           組(ある組) {
             定義(:初期化) {|甲|
@@ -547,7 +543,6 @@ require 'japanize/core'
             読み取り属性 :甲
           }
         }.新規(空の連想配列)
-        警告の出力.clear
 
         等しいことを確認(空の連想配列, ある物.甲)
       }
